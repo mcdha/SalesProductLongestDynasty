d782641495633adb4010bd41bf3d36a9ba85f098
Mc. Dharnel <mcdharnelpagaragan@gmail.com>
2023-05-31 17:33:31 +0800

    final

--- LongestDynasty.js
+++ LongestDynasty.js
@@ -1,42 +1,8 @@
-let dynastyReign = [
-    // { name: "San Dynasty", year: "MXLI",},
-    // { name: "Viloria Dynasty", year: "MCCCIIII",},
-    // { name: "Tan Dynasty", year: "MCCCXCVIII",},
-    // { name: "Bon Dynasty", year: "MCDXLV",},
-    // { name: "Maiko Dynasty", year: "MDCLXIV",},
-    // { name: "Paul Dynasty", year: "MCMXLIX",},
-    // { name: "Andre Dynasty", year: "MMMXICX", },
-];
-
-function longestDynasty()  {
-    if (dynastyReign.length === 0) {
-        return "No Data";
-    }
-
-    let longestReign = 0;
-    let longestDynastyName = "";
-
-    dynastyReign.forEach((currentDynasty) => {
-        let endYear = convertYear(currentDynasty.year);
-
-        if (endYear === "Invalid") {
-            return "Invalid";
-        }
-
-    let reignDuration = endYear - 1000;
-
-        if (reignDuration > longestReign) {
-            longestReign = reignDuration;
-            longestDynastyName = currentDynasty.name;
-        }
-    });
-
-    return longestDynastyName;
-};
-
-
-function convertYear(romanYear){
-    const romanNumerals = {
+function longestDynasty(dynastyReign) {
+    if (dynastyReign.length === 0) return "No Data";
+  
+    const convertYear = (year) => {
+      const numeralMap = {
         M: 1000,
         D: 500,
         C: 100,
@@ -44,22 +10,87 @@ function convertYear(romanYear){
         X: 10,
         V: 5,
         I: 1,
+      };
+  
+      const romanNumeralRegex = /^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/;
+  
+      if (!romanNumeralRegex.test(year)) return { value: year, valid: false };
+  
+      let result = 0;
+      let i = 0;
+  
+      while (i < year.length) {
+        if (numeralMap[year[i]] < numeralMap[year[i + 1]]) {
+          result += numeralMap[year[i + 1]] - numeralMap[year[i]];
+          i += 2;
+        } else {
+          result += numeralMap[year[i]];
+          i += 1;
+        }
+      }
+  
+      return { value: year, valid: true, numeric: result };
     };
-
-    let year = 0;
-    for (let i = 0; i < romanYear.length; i++) {
-        const currentSymbol = romanYear[i];
-        const nextSymbol = romanYear[i + 1];
-            if (romanNumerals[currentSymbol] < romanNumerals[nextSymbol]) {
-                year -= romanNumerals[currentSymbol];
-            } else {
-                year += romanNumerals[currentSymbol];
-            }
+  
+    let longestDuration = 0;
+    let longestDynastyName = "";
+    let longestRomanNumeral = "";
+    let longestConvertedNumber = 0;
+    let invalidDynastyName = "";
+    let invalidRomanNumeral = "";
+  
+    for (let i = 0; i < dynastyReign.length; i++) {
+      const currentDynasty = dynastyReign[i];
+      const endYear = convertYear(currentDynasty.year);
+  
+      if (!endYear.valid) {
+        invalidDynastyName = currentDynasty.name;
+        invalidRomanNumeral = currentDynasty.year;
+        continue;
+      }
+  
+      const startYear = i === 0 ? 1000 : convertYear(dynastyReign[i - 1].year);
+      const duration = endYear.numeric - startYear.numeric;
+  
+      if (duration > longestDuration) {
+        longestDuration = duration;
+        longestDynastyName = currentDynasty.name;
+        longestRomanNumeral = currentDynasty.year;
+        longestConvertedNumber = endYear.numeric;
+      }
     }
-    return year;
-};
-
-
-console.log("Longest reigning dynasty is:", longestDynasty());
-
-
+  
+    return {
+      longest: {
+        name: longestDynastyName,
+        roman: longestRomanNumeral,
+        converted: longestConvertedNumber,
+      },
+      invalid: {
+        name: invalidDynastyName,
+        roman: invalidRomanNumeral,
+      },
+    };
+  }
+  
+  const dynastyReign = [
+    { name: "San Dynasty", year: "MXLI" },
+    { name: "Viloria Dynasty", year: "MCCCIIII" },
+    { name: "Tan Dynasty", year: "MCCCXCVIII" },
+    { name: "Bon Dynasty", year: "MCDXLV" },
+    { name: "Maiko Dynasty", year: "MDCLXIV" },
+    { name: "Paul Dynasty", year: "MCMXLIX" },
+    { name: "Andre Dynasty", year: "MMMXICX" },
+  ];
+  
+  const result = longestDynasty(dynastyReign);
+  
+  console.log("Longest reigning dynasty:");
+  console.log("Name:", result.longest.name);
+  console.log("Roman numeral:", result.longest.roman);
+  console.log("Converted number:", result.longest.converted);
+  
+  console.log("Invalid entry:");
+  console.log("Name:", result.invalid.name);
+  console.log("Roman numeral:", result.invalid.roman);
+  
--- SalesProduct.js
+++ SalesProduct.js
@@ -1,10 +1,10 @@
 const productProfitArray = [
-    // { name: "Product A", profit: -75 },
-    // { name: "Product B", profit: -70 },
-    // { name: "Product C", profit: 100 },
-    // { name: "Product D", profit: 200 },
-    // { name: "Product E", profit: 500 },
-    // { name: "Product F", profit: -1 },
+    { name: "Product A", profit: 1 },
+    { name: "Product B", profit: -70 },
+    { name: "Product C", profit: 100 },
+    { name: "Product D", profit: -1 },
+    { name: "Product E", profit: 500 },
+    { name: "Product F", profit: 2 },
   ];
   
 
@@ -13,7 +13,7 @@ function topProduct(){
       console.log("No Data");
       return;
     }
-    let top = productProfitArray.reduce((prev, curr) => (curr.profit > prev.profit ? curr : prev));
+    let top = productProfitArray.reduce((back, forw) => (forw.profit > back.profit ? forw : back));
     console.log(`Top product = name: ${top.name}, profit: ${top.profit}`);
 }
   
@@ -22,20 +22,36 @@ function bottomProduct() {
       console.log("No Data");
       return;
     }
-    let bottom = productProfitArray.reduce((prev, curr) => (curr.profit < prev.profit ? curr : prev));
+    let bottom = productProfitArray.reduce((back, forw) => (forw.profit < back.profit ? forw : back));
     console.log(`Bottom product = name: ${bottom.name}, profit: ${bottom.profit}`);
 }
   
-function closestToZeroProfitProduct() {
+function closestToPositiveNumberProduct() {
     if (productProfitArray.length === 0) {
       console.log("No Data");
       return;
     }
-    let zero = productProfitArray.reduce((prev, curr) => (Math.abs(curr.profit) < Math.abs(prev.profit) ? curr : prev));
-    console.log(`Zero Profit product = name: ${zero.name}, profit: ${zero.profit}`);
-}
   
-
+    let closest = null;
+  
+    for (let i = 0; i < productProfitArray.length; i++) {
+      const profit = productProfitArray[i].profit;
+  
+      if (profit >= 0 && (closest === null || profit < closest.profit)) {
+        closest = productProfitArray[i];
+      }
+    }
+  
+    if (closest === null) {
+      console.log("No product with a positive profit exists");
+    } else {
+      console.log(`Closest to Zero = name: ${closest.name}, profit: ${closest.profit}`);
+    }
+  }
+  
+  
+  
   topProduct();
   bottomProduct();
-  closestToZeroProfitProduct();
+  closestToPositiveNumberProduct();
+
